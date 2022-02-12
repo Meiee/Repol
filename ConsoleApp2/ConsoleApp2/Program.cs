047997f13cda32cfbed54affef529c55cbbdb1c0
Маргарита <meiee@WIN-JUB8CCCU1GA.DOMAIN.local>
2022-02-12 09:06:29 +0300

    Что-то изменилось

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+
+{
+    public class Program
+    {
+        public static void main(int x)
+        {
+            Console.WriteLine("Введите год");
+            Console.ReadLine();
+            if (x % 4 == 0)
+            {
+                Console.WriteLine("Год високосный");
+            } 
+            else 
+            {
+                Console.WriteLine("Год обычный");
+            };
+
+
+            Console.WriteLine("Haжмитe <Enter> для  завершения программы...");
+            Console.Read();
+        }
+    }
+        
+    
+}
